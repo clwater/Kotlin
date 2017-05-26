f89ea68953a4fca280f1490f5d84448dde06e066
clwater <gengzhibo@gengzhibo.net>
2017-05-26 21:36:03 +0800

    20170526

---
+++ app/src/main/java/com/clwater/kotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.clwater.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.clwater.kotlin.R.id.message
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        message.text = "hellp kotlin"
+    }
+
+}
+
+
